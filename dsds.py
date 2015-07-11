b59723a3d7a96526b85c0be70b4364530a917642
Jason Rice <jsnrice@gmail.com>
2015-07-10 22:23:25 -0400

    Implemented regex substitutions and split. Working as desired.

--- dsds.py
+++ dsds.py
@@ -1,29 +1,57 @@
 #!/usr/bin/python
-import re
+import os
 import random
+import re
  
-number = 8822
-# Open a file
-fo = open("ddginput.txt", "wb")
-for i in xrange(number):
-    # TODO: figure out how to get rid of last comma in file     
-    fo.write(str(random.randint(-3, 3)) + ",")
-# Close opend file
-fo.close()
+# Constants and global members:
+TARGETFILE = "TtLSU"
+LIMIT = 8822
+numbers = [];
 
-# Read in the pdb data file:
-#fstream = open("tester.txt", "r")
-fstream = open("TtLSU.pdb", "r")
+###
+# Generate a bunch of test data values and insert into an array 
+# (list). Don't worry about putting it into a file. Make sure
+# to cast to a string as this will make the injection phase
+# trivial (easy).
+###
+for i in xrange(LIMIT):
+    numbers.insert(i, str(random.randint(-3, 3)))
+
+###
+# Main pdb data file:
+###
+fstream    = open(TARGETFILE+".pdb", "r")
+
+###
+# Temp file for caching injected values:
+###
+fstreamtmp = open(TARGETFILE+".pdb.tmp", "w")
+
+###
+# Convert PDB into a space delimeted format then split
+# on each space. Next inject each number from our list into 
+# each tuple (row) for the 10th column.
+###
+i = 0
 for line in fstream:
-    print line.strip()
-        #line = re.sub(r' +', r' ', line.rstrip())    
+    line = re.sub(r' +', r' ', line.rstrip())    
+    columns = line.rstrip().split(' ');
+    columns[9] = numbers[i]
+    i += 1
+    # Rebuild our string:
+    tuple = ""
+    for word in columns:
+        tuple += word + " "
 
-## with open('test1.txt', 'w') as outf:
-##    for line in inf:
-##        line = line.strip()
-##        if line:
-##            try:
-##                outf.write(int(line, 16))
-##                outf.write('\n')
-##            except ValueError:
-##                print("Could not parse '{0}'".format(line))
+    # Uncomment line below to print each tuple (row) to STDOUT for testing.
+    # print tuple.rstrip()
+
+    # Dump each tuple (row) into the tmp file.
+    # The split removes the extra white space.
+    fstreamtmp.write(str(tuple.rstrip()) + "\n")
+
+fstream.close()
+fstreamtmp.close()
+
+# Superimpose (write over) contents of original pdb with pdb.tmp file:
+os.rename(TARGETFILE+".pdb.tmp", TARGETFILE+".pdb")
